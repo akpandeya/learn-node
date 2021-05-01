c981af0a1661b23616a615d35bd58db9b6729cd2
akpandeya <akpandeya1@gmail.com>
2021-05-01 16:17:25 +0200

    Set up finished

---
+++ README.md
@@ -0,0 +1,3 @@
+# learn-node
+
+I will update the node codes as I lear here
---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text-plain'});
+
+    res.end("hello world")
+}).listen(8081);
+
+console.log("Running on http://localhost:8081");
